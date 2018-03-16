251776b92c6a4ae655c7315babdb303b36b4503e
mrxroot1337 <37379685+mrxroot1337@users.noreply.github.com>
2018-03-16 08:32:45 -0500

    Add files via upload

---
+++ Quizletbot.js
@@ -0,0 +1,18 @@
+$("button.UIButton.UIButton--hero").trigger("click");
+var terms = $("a.cell[data-type=term]"), definitions = $("a.cell[data-type=definition]");
+setInterval(function() {
+  for (var b = 0;b < terms.length;b++) {
+    toEng($(terms[b]).children().children().html()).trigger("click"), $(terms[b]).trigger("click");
+  }
+}, 1);
+function toEng(b) {
+  console.log("word: " + b);
+  for (var c, a = 0;a < terms.length;a++) {
+    $(terms[a]).children().children().html() == b && (c = $(terms[a]).attr("data-id"));
+  }
+  for (a = 0;a < definitions.length;a++) {
+    if ($(definitions[a]).attr("data-id") == c) {
+      return $(definitions[a]);
+    }
+  }
+};
